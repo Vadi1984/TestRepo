fe046fe1832c52d41c396904d9bad2be37ea4898
vadiraja <vadi_raja@hotmail.com>
2024-09-04 07:02:48 +0530

    commit -m "changed the file"

--- new1.java
+++ new1.java
@@ -1,7 +1,8 @@
-public class Test
+public class new1
 {
 	public static void main (String[] args)
 	{
 			System.out.println("This is Test Program");
+			System.out.println("This is for Testing");
 	}
 }
